271e5aa2da33ad8c28399beac652976695b17662
ccc700 <700eeu@gmail.com>
2026-04-07 04:32:42 +0900

    프로젝트 생성

---
+++ theway/src/test/java/com/navi/theway/ThewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.navi.theway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
